2fc9a77bed468d1dd07e5c16f7faa75737f1beb8
unknown <praveena.ma12@gmail.com>
2019-02-01 19:37:28 +0530

    mjhg

--- python1/day3/render2.py
+++ python1/day3/render2.py
@@ -1,10 +1,19 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
+def login():
+   return render_template('log.html')
+@app.route('/home/')
+
 def home():
     return render_template('home2.html')
 @app.route('/about/')
+
 def about():
    return render_template('about2.html')
+@app.route('/contact/')
+
+def contact():
+   return render_template('contact.html')
 if __name__=='__main__':
      app.run()
