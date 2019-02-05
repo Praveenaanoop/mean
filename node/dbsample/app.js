195b39765d90ae63b0de5df86a1aadd4f28e50a3
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-02-05 11:02:00 +0530

    msgsss

--- node/dbsample/app.js
+++ node/dbsample/app.js
@@ -8,22 +8,115 @@ app.use(bodyparser.urlencoded({extended:true}))
 
 app.set("view engine","ejs")
 app.get("/",function(req,res){
-    res.render("home")
+    res.render("emphome");
 })
+app.get("/newemp",function(req,res){
+    res.render("home");
+})
+app.get("/edit",function(req,res){
+    mongoclient.connect(url,function(err,database){
+        if(err){
+            res.send("error");
+        }
+        else{
+            var dtb=database.db('mydb');
+    var empcol=dtb.collection('employee');
+    empcol.find({},{id:1}).toArray(function(err,tbdata){
+      if(err){
+          res.send(err);
+      }
+      else{
+          res.render("empedit",{data:tbdata});
+      }
+    })
+        }
+    })
+})
+app.get("/view",function(req,res){
+   mongoclient.connect(url,function(err,database){
+       if(err){console.log(err);}
 
-
+else{
+    var dtb=database.db('mydb');
+    var empcol=dtb.collection('employee');
+    empcol.find().toArray(function(err,tbdata){
+      res.render("empview",{data:tbdata});
+      console.log(tbdata);
+    })
+}    })
+})
+app.get("/del",function(req,res){
+    mongoclient.connect(url,function(err,database){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var dtb=database.db('mydb');
+            var empcol=dtb.collection('employee');
+            empcol.find({},{name:1}).toArray(function(err,tbdata){
+                res.render("empdel",{data:tbdata});
+            })
+        }
+    })
+})
+app.post("/empty",function(req,res){
+    mongoclient.connect(url,function(err,database){
+        var dtb=database.db('mydb');
+        var empcol=dtb.collection('employee');
+        empcol.update({id:parseInt(req.body.id)},{$set:{name:req.body.name,salary:req.body.sal}})
+        if(err)
+        {
+            res.send(err);
+        }
+        else{
+            res.send("succsefully updated");
+        }
+    })
+})
+app.post("/empedit",function(req,res){
+    console.log(req.body.editbox);
+    mongoclient.connect(url,function(err,database){
+        var dtb=database.db('mydb');
+        var empcol=dtb.collection('employee');
+        empcol.find({id:parseInt(req.body.editbox)}).toArray(function(err,tbdata){
+            if(err)
+            {
+                res.send("error");
+            }
+            else{
+                res.render("empty",{data:tbdata});
+                console.log(tbdata);
+            }
+        })
+    })
+})
+app.post("/empdel",function(req,res){
+    console.log(req.body.delbox);
+    mongoclient.connect(url,function(err,database){
+    var dtb=database.db('mydb');
+    var empcol=dtb.collection('employee');
+    empcol.deleteOne({id:parseInt(req.body.delbox)},function(err,result){
+        if(err){
+            res.send("error");
+        }
+        else{
+            res.send("successfully deleted");
+        }
+    })    
+    })
+})
 app.post("/insert",function(req,res){
     mongoclient.connect(url,function(err,database){
         var dtb=database.db('mydb');
         var empcol=dtb.collection('employee');
-        var data={"name":req.body.emp,"salary":req.body.sal}
+        var data={"id":req.body.id,"name":req.body.emp,"salary":req.body.sal}
     
         empcol.insert(data,function(err,result){
             if(err){
-                res.send("something wend wrong")
+                res.send("something went wrong");
             }
             else{
-                res.send("inserted successfully")
+                res.send("inserted successfully");
             }
         })
     })
